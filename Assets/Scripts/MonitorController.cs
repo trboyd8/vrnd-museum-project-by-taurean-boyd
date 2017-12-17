2a26f9579d17e16efb21463b31df64ed9464ff64
Taurean Boyd <trboyd8@gmail.com>
2017-12-17 17:25:15 -0500

    Creating the first exhibit.

---
+++ Assets/Scripts/MonitorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MonitorController : MonoBehaviour {
+
+	private bool isRunning = false;
+
+	public void Run() {
+		VideoPlayer player = this.GetComponent<VideoPlayer> ();
+		if (!isRunning) {
+			player.Play ();
+			isRunning = true;
+		} else {
+			player.Pause ();
+			isRunning = false;
+		}
+	}
+}
